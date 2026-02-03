7145397add68583fe68b56529b9edb16ca0bdde0
kashish-238 <kashishdhanani3@gmail.com>
2026-02-03 07:33:58 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files from public folder
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
